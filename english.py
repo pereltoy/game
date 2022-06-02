d6164371e2e60d9cf2039b220bb059f5d12194b3
emailmeydrive <emailme.ydrive@gmail.com>
2022-06-02 11:49:14 +0300

    hyi

--- english.py
+++ english.py
@@ -105,7 +105,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
+
             x_mouse = pos[0]
+            print(x_mouse)
             y_mouse_pos = pos[1]
             l = x_mouse//42
             word = mouse_click_pos_latter(y_mouse_pos, l)
@@ -155,9 +157,9 @@ while running:
         screen.blit(img, (15+l*42, 550))
     screen.blit(cursor_image, (cursor_x, 561))
     # if finger13_y>0.75 and finger13_y<1:
-    if finger13_x < 1 and finger13_x > 0.65:
+    if finger13_x < 1 and finger13_x > 0.65 and cursor_x >= 0:
         cursor_x -= 42
-    if finger13_x < 0.45 and finger13_x > 0:
+    if finger13_x < 0.45 and finger13_x > 0 and cursor_x <= 1100:
         cursor_x += 42
         # if
         # for i in jsonData:
